130ad187d75d7abfa2c2e4a630ae7b1ef370d675
Cooltharani <141839325+Cooltharani@users.noreply.github.com>
2023-08-23 17:08:16 +0530

    color

---
+++ Calc Window1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ATM
+{
+    /// <summary>
+    /// Interaction logic for Calc_Window1.xaml
+    /// </summary>
+    public partial class Calc_Window1 : Window
+    {
+        public Calc_Window1()
+        {
+            InitializeComponent();
+        }
+    }
+}
